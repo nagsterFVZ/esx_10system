d04d537fbd0aca52f115a03f7aa7db8a1031e398
nagsterFVZ <nagsterza@gmail.com>
2020-05-21 15:03:21 +0200

    DB table primary key

--- 10system.sql
+++ 10system.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS `10system` (
-  `identifier` varchar(100) NOT NULL,
-  `callsign` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL
-)
+   `identifier` varchar(100) NOT NULL,
+   `callsign` varchar(100) NOT NULL,
+   `name` varchar(100) NOT NULL,
+   
+   PRIMARY KEY (identifier)
+ )
